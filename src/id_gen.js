9de9c8958538f6a89aec1c113e1137f8bbfdc78b
Lyle Underwood <lyleunderwood@gmail.com>
2015-05-02 16:20:00 -0600

    fixed up id_gen

--- src/id_gen.js
+++ src/id_gen.js
@@ -2,12 +2,10 @@ var crypto = require('crypto');
 var md5 = crypto.createHash('md5');
 
 module.exports = function() {
+  var args = [].concat.apply([], arguments);
+  var hashVar = args + '_' + (+new Date()) + '_' + (Math.random() * 1000);
 
-	var args = [].concat.apply([], arguments);
-	var hashVar = args + '_' + +new Date(); + '_' + (Math.random() * 1000);
+  md5.update(hashVar);
 
-	md5.update(hashVar);
-
-	return md5.digest('base64');
-
+  return md5.digest('base64');
 };
