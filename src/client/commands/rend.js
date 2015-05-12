28e5cbb8e4f1b6137bb7008a36548296dce24cf3
Casey Clyde <caseysclyde@gmail.com>
2015-05-11 23:48:33 -0700

    rend work

--- src/client/commands/rend.js
+++ src/client/commands/rend.js
@@ -1,5 +1,15 @@
-CommandDisplay.prototype.renderers['rend'] = function(response) {
-  var disp = '';
+CommandDisplay.prototype.renderers['rend'] = function(resp) {
+  var disp = 'REND: Port will be rent along the specified attack vector...<br/>';
+             
 
-    return disp;
+  if(!!resp) {
+
+    disp += 'Computing port velocities... ' + Math.round(resp.response.progress * 100) + '%<br/>';
+
+    if(resp.status === 'success') {
+      disp += 'REND: Port has been rent. Returned response:';
+    } 
+  }
+
+  return disp;
 };
